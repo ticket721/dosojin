6fcd47b83b3de1c77c541fd2dc8d69dae9c48eb4
Goldiggy <jeremy.goldsztejn@gmail.com>
2020-01-13 19:19:26 +0100

    test: create mock entities
    Create SimpleConnector, SimpleReceptacle and SimpleOperation for testing
    purposes

---
+++ sources/mocks/operation/SimpleOperationMock.ts
@@ -0,0 +1,24 @@
+import { Dosojin, Gem, Operation } from "../../core";
+
+export class SimpleOperationMock extends Operation {
+
+    constructor(name: string, dosojin: Dosojin) {
+        super(name, dosojin);
+    }
+
+    public async run(gem: Gem): Promise<Gem> {
+        return null;
+    }
+
+    public async dryRun(gem: Gem): Promise<Gem> {
+        return null;
+    }
+
+    public async cost(gem: Gem): Promise<Gem> {
+        return null;
+    }
+
+    public async scopes(gem: Gem): Promise<string[]> {
+        return null;
+    }
+}
