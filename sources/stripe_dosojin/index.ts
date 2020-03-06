59f3ce803324237227a801872066a5917bab0268
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-03-06 10:33:58 +0100

    fix: export region and method restriction types

--- sources/stripe_dosojin/index.ts
+++ sources/stripe_dosojin/index.ts
@@ -4,8 +4,8 @@ export const stripe = new Stripe('process.env.STRIPE_SECRET_KEY_TEST', {
     apiVersion: '2019-12-03',
 });
 
-export { GenericStripeDosojin } from './GenericStripeDosojin';
-export { CardPaymentIntentReceptacle } from './CardPaymentIntentReceptacle';
-export { SepaDebitPaymentIntentReceptacle } from './SepaDebitPaymentIntentReceptacle';
-export { CardPayoutConnector } from './CardPayoutConnector';
-export { BankAccountPayoutConnector } from './BankAccountPayoutConnector';
+export { GenericStripeDosojin }                                                              from './GenericStripeDosojin';
+export { CardPaymentIntentReceptacle, PaymentMethodRestrictions, PaymentRegionRestrictions } from './CardPaymentIntentReceptacle';
+export { SepaDebitPaymentIntentReceptacle }                                                  from './SepaDebitPaymentIntentReceptacle';
+export { CardPayoutConnector }                                                               from './CardPayoutConnector';
+export { BankAccountPayoutConnector }                                                        from './BankAccountPayoutConnector';
